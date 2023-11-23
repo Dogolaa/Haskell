5abf009f2cfd92f0632a5996b29a4d27e1905061
Dogolaa <lucasdogo01@gmail.com>
2023-11-23 13:51:18 -0300

    Atualizando algumas funcoes

--- aula6.hs
+++ aula6.hs
@@ -7,26 +7,16 @@ primeirosN n (x:xs) = x : primeirosN (n-1) xs
 --Exercicio 2
 
 existeNumero :: Eq a => a -> [a] -> Bool
-existeNumero _ [] = False  
+existeNumero _ [] = False 
 existeNumero num (x:xs)
     | num == x  = True      
     | otherwise = existeNumero num xs
 
 --Exercicio 3
 
-maiorValor :: (Ord a, Num a) => a -> [a] -> a
-maiorValor _ [] = 0 
-maiorValor padrao (x:xs) = foldl max x xs
-
-
-foldl :: (a -> b -> a) -> a -> [b] -> a
-foldl f acc [] = acc
-foldl f acc (x:xs) = foldl f (f acc x) xs
-
-
-max :: (Ord a) => a -> a -> a
-max a b | a >= b    = a
-        | otherwise = b
+maiorValor:: [Int] -> Int
+maiorValor [a] = a 
+maiorValor (a:x) = max a (maiorValor x)
 
 
 --Exercicio 4
@@ -40,20 +30,15 @@ inverterAux (x:xs) acc = inverterAux xs (x:acc)
 
 --Exercicio 5
 
-ultimoElemento :: a -> [a] -> a
-ultimoElemento padrao [] = padrao
-ultimoElemento _ [x] = x
-ultimoElemento padrao (_:xs) = ultimoElemento padrao xs
+ultimoElemento:: [Int] -> Int
+ultimoElemento [a] = a 
+ultimoElemento (a:x) = ultimoElemento x
+
 
 --Exercicio 6
 
-kEsimoElemento :: Int -> a -> [a] -> a
-kEsimoElemento k padrao lista
-    | k < 0 = padrao  
-    | otherwise = encontraK k lista
+kEsimoElemento:: [Int] -> Int -> Int
+kEsimoElemento (a:x) n 
+    | n == 1 = a 
+    | otherwise = kEsimoElemento x (n-1)
 
-encontraK :: Int -> [a] -> a
-encontraK 0 (x:_) = x             
-encontraK k (_:xs)
-    | k > 0 = encontraK (k - 1) xs  
-encontraK _ [] = error "Índice fora dos limites da lista"
